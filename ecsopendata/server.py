2561d56032829ecf50679a951f7c5d6351ad54b4
Matt Murbach <mmurbach@uw.edu>
2018-10-02 14:42:52 -0500

    completely refactor everything

---
+++ ecsopendata/server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore
+from database import db_session, init_db
+from models import User, Role
+
+# Create app
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_PASSWORD_SALT'] = '0'
+
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(db_session,
+                                                User, Role)
+security = Security(app, user_datastore)
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    init_db()
+    # user_datastore.create_user(email='admin', password='password')
+    db_session.commit()
+
+# Views
+@app.route('/')
+@login_required
+def home():
+    return 'Here you go!'
+
+if __name__ == '__main__':
+    app.run()
